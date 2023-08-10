5ca00f0c1f4d0e61d5b064ed2e79d72f1fe26fed
Nelson Steve <nelsonsteve799@gmail.com>
2023-08-10 17:05:04 +0500

    implementing vec3 class

--- RayTracing/src/Math/vec3.h
+++ RayTracing/src/Math/vec3.h
@@ -8,31 +8,31 @@ namespace Math {
 	class vec3 {
 	public:
 		vec3() {}
-		vec3(real x, real y, real z) { e[0] = x, e[1] = y, e[2] = z; }
-		inline real x() const { return e[0]; }
-		inline real y() const { return e[1]; }
-		inline real z() const { return e[2]; }
-		inline real r() const { return e[0]; }
-		inline real g() const { return e[1]; }
-		inline real b() const { return e[2]; }
+		vec3(real x, real y, real z) { vec[0] = x, vec[1] = y, vec[2] = z; }
+		inline real x() const { return vec[0]; }
+		inline real y() const { return vec[1]; }
+		inline real z() const { return vec[2]; }
+		inline real r() const { return vec[0]; }
+		inline real g() const { return vec[1]; }
+		inline real b() const { return vec[2]; }
 
 		inline const vec3& operator+() const { return *this; }
-		inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-		inline real operator[](int i) const { return e[i]; }
-		inline real& operator[](int i) { return e[i]; }
+		inline vec3 operator-() const { return vec3(-vec[0], -vec[1], -vec[2]); }
+		inline real operator[](int i) const { return vec[i]; }
+		inline real& operator[](int i) { return vec[i]; }
 
-		inline vec3& operator+=(const vec3& v2);
-		inline vec3& operator-=(const vec3& v2);
-		inline vec3& operator*=(const vec3& v2);
-		inline vec3& operator/=(const vec3& v2);
-		inline vec3& operator*=(const real v2);
-		inline vec3& operator/=(const real v2);
+		inline vec3& operator+=(const vec3& vec2);
+		inline vec3& operator-=(const vec3& vec2);
+		inline vec3& operator*=(const vec3& vec2);
+		inline vec3& operator/=(const vec3& vec2);
+		inline vec3& operator*=(const real value);
+		inline vec3& operator/=(const real value);
 
-		inline real length() const { return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]); }
-		inline real squared_length() const { return e[0] * e[0] + e[1] * e[1] + e[2] * e[2]; }
-		inline void MakeUnitVector();
+		inline real length() const { return sqrt(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2]); }
+		inline real squared_length() const { return vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2]; }
+		inline vec3 MakeUnitVector(vec3& v);
 
 	private:
-		real e[3];
+		real vec[3];
 	};
 }
