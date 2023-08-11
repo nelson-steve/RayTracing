0cdf4d50b2257fa9a4e50bce8ad846d3fb6c1739
Nelson Steve <nelsonsteve799@gmail.com>
2023-08-12 00:49:17 +0500

    created a randomly generated scene + completed Ray Tracing in One Weekend book

--- RayTracing/src/main.cpp
+++ RayTracing/src/main.cpp
@@ -30,24 +30,67 @@ Math::vec3 color(const ray& r, hitable* world, int depth) {
 	}
 }
 
+hitable* random_scene() {
+	int n = 500;
+	hitable** list = new hitable * [n + 1];
+	list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+	int i = 1;
+	for (int a = -11; a < 11; a++) {
+		for (int b = -11; b < 11; b++) {
+			real random_number = ((real)rand() / (RAND_MAX));
+			real choose_mat = random_number;
+			real rand1 = ((real)rand() / (RAND_MAX));
+			real rand2 = ((real)rand() / (RAND_MAX));
+			vec3 center(a + 0.9 * rand1, 0.2, b + 0.9 * rand2);
+			if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+				if (choose_mat < 0.8) {
+					rand1 = ((real)rand() / (RAND_MAX));
+					rand2 = ((real)rand() / (RAND_MAX));
+					real rand3 = ((real)rand() / (RAND_MAX));
+					real rand4 = ((real)rand() / (RAND_MAX));
+					real rand5 = ((real)rand() / (RAND_MAX));
+					real rand6 = ((real)rand() / (RAND_MAX));
+					list[i++] = new sphere(center, 0.2, new lambertian(vec3(rand1 * rand2, rand3 * rand4, rand5 * rand6)));
+				}
+				else if (choose_mat < 0.95) {
+					rand1 = ((real)rand() / (RAND_MAX));
+					rand2 = ((real)rand() / (RAND_MAX));
+					real rand3 = ((real)rand() / (RAND_MAX));
+					list[i++] = new sphere(center, 0.2, new metal(vec3(0.5 * (1 + rand1), 0.5 * (1 + rand2), 0.5 * (1 + rand3))));
+				}
+			}
+			else {
+				list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+			}
+		}
+	}
+	list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+	list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+	list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5)));
+
+	return new hitable_list(list, i);
+}
+
 int main(int argc, char** argv[]) {
-	int multiple = 1;
+	int multiple = 4;
 	int nx = multiple * 200;
 	int ny = multiple * 100;
 	int ns = multiple * 100;
 	std::ofstream img("picture.ppm");
-	hitable* list[6];
-	list[0] = new sphere(vec3(0, 0, -2), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
-	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
-	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
-	list[4] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
-	list[5] = new sphere(vec3(0, 0.0, -1), 0.5, new lambertian(vec3(0.5, 0.0, 0.5)));
-	hitable* world = new hitable_list(list, 6);
-	vec3 lookfrom(3, 3, 2);
-	vec3 lookat(0, 0, -1);
-	real dist_to_focus = (lookfrom - lookat).length();
-	real aperture = 2.0;
+	//hitable* list[6];
+	//list[0] = new sphere(vec3(0, 0, -2), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
+	//list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+	//list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
+	//list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
+	//list[5] = new sphere(vec3(0, 0.0, -1), 0.5, new lambertian(vec3(0.5, 0.0, 0.5)));
+	//list[4] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
+	//hitable* world = new hitable_list(list, 6);
+	hitable* world = random_scene();
+	vec3 lookfrom(13, 2, 3);
+	vec3 lookat(0, 0, 0);
+	//real dist_to_focus = (lookfrom - lookat).length();
+	real dist_to_focus = 10.0;
+	real aperture = 0.1;
 	camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, real(nx) / real(ny), aperture, dist_to_focus);
 	if (img.is_open()) {
 		img << "P3" << std::endl << nx << " " << ny << std::endl << "255" << std::endl;
